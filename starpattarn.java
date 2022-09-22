3d91ce0bd8d360b5f9245265a4b9b665246860ba
thakurlal123 <114184268+thakurlal123@users.noreply.github.com>
2022-09-23 01:03:21 +0530

    Add files via upload

---
+++ starpattarn.java
@@ -0,0 +1,17 @@
+public class starpattarn {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            //inner loop for printing spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //inner loop for printing star
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
